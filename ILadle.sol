6fbbc11284191fda4e2200edb054217c94d42464
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-03-23 15:27:30 +0000

    2.0.9: ladle._join to settle

--- ILadle.sol
+++ ILadle.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.0;
 
 
 interface ILadle {
-    /// @dev Allow authorized contracts to move assets through the ladle
-    function _join(bytes12 vaultId, address user, int128 ink, int128 art) external;
+    /// @dev Allow liquidation contracts to move assets to wind down vaults
+    function settle(bytes12 vaultId, address user, int128 ink, int128 art) external;
 }
