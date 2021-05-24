09971c0a5fd6189ce4522c6d68cba12dec01cb86
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-05-24 18:28:02 +0100

    2.0.31: Gov interfaces

---
+++ ILadleGov.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+interface ILadleGov {
+    function joins(bytes6) external view returns (IJoin);
+    function addJoin(bytes6, address) external;
+    function addPool(bytes6, address) external;
+}
