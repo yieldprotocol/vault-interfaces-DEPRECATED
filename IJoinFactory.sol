099c9c4e434abdfa6d6ef8b9340532f9704f9a1b
uivlis <vladsilviufarcas@gmail.com>
2021-06-18 13:21:57 +0300

    Drop CREATE2 functions (in favor of CREATE)

--- IJoinFactory.sol
+++ IJoinFactory.sol
@@ -5,9 +5,6 @@ pragma solidity >= 0.8.0;
 interface IJoinFactory {
   event JoinCreated(address indexed asset, address pool);
 
-  function JOIN_BYTECODE_HASH() external pure returns (bytes32);
-  function calculateJoinAddress(address asset) external view returns (address);
-  function getJoin(address asset) external view returns (address);
   function createJoin(address asset) external returns (address);
   function nextAsset() external view returns (address);
 }
